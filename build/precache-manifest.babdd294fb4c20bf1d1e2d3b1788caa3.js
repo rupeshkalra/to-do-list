632dea9f73253b31e36ff7585e99dd896a921253
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-03-03 15:25:54 +0530

    done

---
+++ build/precache-manifest.babdd294fb4c20bf1d1e2d3b1788caa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1ed2aa17d989b4590f50b9374f15a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c15be61b483033d6aec7",
+    "url": "/static/css/main.fcb169c6.chunk.css"
+  },
+  {
+    "revision": "d1f26b83a65489b7e33c",
+    "url": "/static/js/2.f4eeaf50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4eeaf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15be61b483033d6aec7",
+    "url": "/static/js/main.1a07f0e0.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state=
+    {
+      newItem: "",
+      list: []
+    }
+  };
+
+  addItem(car){
+    if(car!== ""){
+      const newItem={
+        id: Date.now(),
+        value: car,
+        isDone: false
+      };
+      const list=[...this.state.list];
+      list.push(newItem);
+      
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  }
+
+  deletecar(id){
+    const list=[...this.state.list];
+    const updatedlist = list.filter(item=>item.id !==id);
+    this.setState({
+      list: updatedlist
+    })
+  }
+  
+  updateInput(input){
+    this.setState({newItem:input});
+  }
+
+  render(){
+  return(
+    <div>
+          <h1 className="app-title">To Do :</h1>
+          <div className="Add">
+                Make an addition to the List:
+                <br/>
+                <input 
+                type="text" 
+                className="input-text"
+                placeholder="Write here"
+                required
+                value={this.state.newItem}
+                onChange={e => this.updateInput(e.target.value)}
+                />
+          <button className="add-btn" type="submit" onClick={()=> this.addItem(this.state.newItem)}>Add Item</button>
+          </div>
+          <br/>
+          <div className="listing">
+              <ul>
+                {this.state.list.map(item =>{
+                  return (
+                    <div key={item.id}className="cont">
+                      <li key={item.id}>
+                        {item.value}
+                      </li>
+                      <button className="btn" onClick={()=>{this.deletecar(item.id)}}>Done</button>
+                    </div>
+                  );
+                })}
+              </ul>
+          </div>
     </div>
   );
+  }
+
 }
 
 export default App;
